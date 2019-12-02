0ebd748f1c4f9b59772c3379795621221fa539b4
allnichols <allenmaxnichols@gmail.com>
2019-12-02 08:35:11 -0600

    added exercises

---
+++ types/types.js
@@ -0,0 +1,14 @@
+//3 pillars Types, Scope, Objects (Oriented) System
+//Types
+// Not everything in JavaScript is ana object
+//PRIMITIVE TYPES
+// undefined, string, number, boolean, object, symbol,
+//-undeclared?, -null?, -function?, -array?, -bigint?
+//Variables don't have types, values do.
+var v;
+console.log(typeof v);
+//Special Values
+//NaN - special value for 'invalid number'
+
+//Negative Zero
+// -0
