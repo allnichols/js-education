a3e0db4bbe63e23c09512959d12aea7080177c9e
allnichols <allenmaxnichols@gmail.com>
2019-12-04 08:59:56 -0600

    wokred on equality

---
+++ equality/equality.js
@@ -0,0 +1,5 @@
+// when types match in '==' js uses '==='
+// if types are diff in '===' it returns false then so on and so forth
+
+// == allows coercion (types different)
+// === disallows coercion (types same)
