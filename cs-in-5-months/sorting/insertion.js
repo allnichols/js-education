76b4fd3faa1c06d2dc01500178f718712f9e40ec
allnichols <allenmaxnichols@gmail.com>
2019-12-09 08:46:47 -0600

    added insertion algorithm

--- cs-in-5-months/sorting/insertion.js
+++ cs-in-5-months/sorting/insertion.js
@@ -6,3 +6,20 @@
 
 //   Like bubble sort, there's a visualization mechanism available to you. Just call snapshot(myArray) at the beginning of
 //   your inner loop and it should handle the rest for you!
+var insertionSort = nums => {
+  let arr = [];
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] < nums[j]) {
+        let spliced = nums.splice(i, 1);
+        nums.splice(j, 0, spliced[0]);
+        arr.push(nums);
+      }
+    }
+  }
+  return arr;
+};
+
+var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+
+console.log(insertionSort(nums));
