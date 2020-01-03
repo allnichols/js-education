40019c1ac1b1cedc9600ac15c3b80205a1dc151a
allnichols <allenmaxnichols@gmail.com>
2020-01-03 14:52:10 -0600

    finishing up course

--- regex/regex.js
+++ regex/regex.js
@@ -2,3 +2,10 @@ var msg = "Hello World";
 
 //assertions, look ahead
 console.log(msg.match(/(l.)/g));
+console.log(msg.match(/(l.)$/g));
+console.log(msg.match(/(l.)(?=o)/g));
+console.log(msg.match(/(l.)(?!o)/g));
+
+// Look behind
+console.log(msg.match(/(?<=e)(l.)/g));
+console.log(msg.match(/(?<!e)(l.)/g));
